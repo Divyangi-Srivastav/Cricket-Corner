fcc5a4c1190ac17a2ada7d2bbb08f467949de456
Ansh <anshtandonat@gmail.com>
2021-08-30 21:09:16 +0530

    Day 1 : added code to save host details to firebase and generate tournament Id

--- app/src/main/java/com/example/livecricketapp/activities/Host_a_tournament.java
+++ app/src/main/java/com/example/livecricketapp/activities/Host_a_tournament.java
@@ -7,21 +7,70 @@ import android.view.View;
 
 import com.example.livecricketapp.R;
 import com.example.livecricketapp.databinding.ActivityHostAtournamentBinding;
+import com.example.livecricketapp.model.TournamentHostInfo;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Host_a_tournament extends AppCompatActivity {
 
     private ActivityHostAtournamentBinding binding;
+    private DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHostAtournamentBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("Tournament Host Info");
     }
 
-    
 
-    public void back ( View view )
-    {
+    public void back(View view) {
         finish();
     }
+
+    public void clear(View view) {
+
+        switch (view.getId()) {
+            case R.id.name_cross:
+                binding.name.getText().clear();
+                break;
+            case R.id.upi_id_cross:
+                binding.upiId.getText().clear();
+                break;
+            case R.id.phone_cross:
+                binding.phone.getText().clear();
+                break;
+            case R.id.address_cross:
+                binding.address.getText().clear();
+                break;
+
+        }
+    }
+
+    public void create_tournament ( View view  )
+    {
+        TournamentHostInfo tournamentHostInfo = new TournamentHostInfo();
+        tournamentHostInfo.setHostName(binding.name.getText().toString());
+        tournamentHostInfo.setUpiId(binding.upiId.getText().toString());
+        tournamentHostInfo.setPhoneNumber(binding.phone.getText().toString());
+        tournamentHostInfo.setAddress(binding.address.getText().toString());
+
+        tournamentHostInfo.setTournamentId(generate_tournament_Id());
+
+        push_data_to_firebase(tournamentHostInfo);
+    }
+
+    public String generate_tournament_Id ()
+    {
+        long time = System.currentTimeMillis();
+        return String.valueOf(time);
+    }
+
+    public void push_data_to_firebase( TournamentHostInfo tournamentHostInfo )
+    {
+        databaseReference.push().setValue(tournamentHostInfo);
+    }
+
 }
--- app/src/main/java/com/example/livecricketapp/model/TournamentHostInfo.java
+++ app/src/main/java/com/example/livecricketapp/model/TournamentHostInfo.java
@@ -8,17 +8,27 @@ public class TournamentHostInfo implements Serializable {
     private String upiId;
     private String phoneNumber;
     private String address;
+    private String TournamentId;
 
     public TournamentHostInfo ()
     {
 
     }
 
-    public TournamentHostInfo(String hostName, String upiId, String phoneNumber, String address) {
+    public TournamentHostInfo(String hostName, String upiId, String phoneNumber, String address, String tournamentId) {
         this.hostName = hostName;
         this.upiId = upiId;
         this.phoneNumber = phoneNumber;
         this.address = address;
+        TournamentId = tournamentId;
+    }
+
+    public String getTournamentId() {
+        return TournamentId;
+    }
+
+    public void setTournamentId(String tournamentId) {
+        TournamentId = tournamentId;
     }
 
     public String getHostName() {
