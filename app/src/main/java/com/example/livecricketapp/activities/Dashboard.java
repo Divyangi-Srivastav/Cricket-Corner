bcd93367ce48b8bf06ecfa06a56a00b26edd95bf
Ansh <anshtandonat@gmail.com>
2021-09-03 19:33:41 +0530

    Day 5 : Completed UI for Dashboard

--- app/src/main/java/com/example/livecricketapp/activities/Dashboard.java
+++ app/src/main/java/com/example/livecricketapp/activities/Dashboard.java
@@ -1,22 +1,29 @@
 package com.example.livecricketapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.livecricketapp.R;
+import com.example.livecricketapp.adapters.DashboardAdapter;
 import com.example.livecricketapp.databinding.ActivityDashboardBinding;
 
-public class Dashboard extends AppCompatActivity {
+public class Dashboard extends AppCompatActivity implements DashboardAdapter.Move_Next {
 
     private ActivityDashboardBinding binding;
+    private DashboardAdapter dashboardAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityDashboardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        dashboardAdapter = new DashboardAdapter(this,this::move_to_other_activity);
+        binding.recyclerView.setAdapter(dashboardAdapter);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public void back(View view) {
@@ -26,5 +33,11 @@ public class Dashboard extends AppCompatActivity {
     public void edit_content ( View view )
     {
 
+    }
+
+    @Override
+    public void move_to_other_activity(int a) {
+        Intent intent = new Intent(this,TournamentSettings.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/livecricketapp/adapters/DashboardAdapter.java
+++ app/src/main/java/com/example/livecricketapp/adapters/DashboardAdapter.java
@@ -4,19 +4,24 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.livecricketapp.R;
 
 import org.jetbrains.annotations.NotNull;
 
 public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.DashboardViewHolder> {
 
     private LayoutInflater layoutInflater;
+    private Move_Next move_next;
 
-    public DashboardAdapter ( Context context )
+    public DashboardAdapter ( Context context , Move_Next move_next )
     {
-        layoutInflater = LayoutInflater.from(context);
+        this.layoutInflater = LayoutInflater.from(context);
+        this.move_next = move_next;
     }
 
 
@@ -24,7 +29,8 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
     @NotNull
     @Override
     public DashboardViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+        View view = layoutInflater.inflate(R.layout.recycler_view_dashboard,parent,false);
+        return new DashboardViewHolder(view);
     }
 
     @Override
@@ -34,12 +40,28 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 2;
     }
 
     public class DashboardViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name , host_name , view_more;
+
         public DashboardViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
+            name = itemView.findViewById(R.id.tournament_name);
+            host_name = itemView.findViewById(R.id.tournament_host_name);
+            view_more = itemView.findViewById(R.id.view_more);
+            view_more.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    move_next.move_to_other_activity(getAdapterPosition());
+                }
+            });
         }
+    }
+
+    public interface Move_Next{
+        void move_to_other_activity( int a );
     }
 }
