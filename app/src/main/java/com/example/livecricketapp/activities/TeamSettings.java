87aad1aa6240e1fce1b8bc69d4d87f018fa7fb67
Ansh <anshtandonat@gmail.com>
2021-09-06 17:42:22 +0530

    Day 7 : Completed UI for team Settings

--- app/src/main/java/com/example/livecricketapp/activities/TeamSettings.java
+++ app/src/main/java/com/example/livecricketapp/activities/TeamSettings.java
@@ -6,20 +6,47 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.livecricketapp.databinding.ActivityTeamSettingsBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class TeamSettings extends AppCompatActivity {
 
     private ActivityTeamSettingsBinding binding;
+    private FirebaseFirestore db;
+    private String tournamentId = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTeamSettingsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        tournamentId = getIntent().getStringExtra("id");
+    }
+
+    public void edit ( View view )
+    {
+
+    }
+
+    public void set_paid ( View view )
+    {
+
+    }
+
+    public void set_unpaid ( View view )
+    {
+
     }
 
     public void back(View view) {
         finish();
+    }
+
+    public void get_data ()
+    {
+        db.collection("Tournament Team Info")
+                .document(tournamentId)
+                .get()
+                .addOnSuccessListener()
     }
 
 }
