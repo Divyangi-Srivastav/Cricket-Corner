8115747621f2030203af604515eb4707f5f2917d
Ansh <anshtandonat@gmail.com>
2021-09-18 17:30:39 +0530

    Day 18 : Completed the matches adapter and also the Teams and Matches Activity for User

--- app/src/main/java/com/example/livecricketapp/user/activities/TeamsAndMatches.java
+++ app/src/main/java/com/example/livecricketapp/user/activities/TeamsAndMatches.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -14,20 +15,23 @@ import com.example.livecricketapp.activities.AdsRequest;
 import com.example.livecricketapp.activities.Dashboard;
 import com.example.livecricketapp.activities.HomeActivity;
 import com.example.livecricketapp.databinding.ActivityTeamsAndMatchesBinding;
+import com.example.livecricketapp.model.AllMatchInfo;
 import com.example.livecricketapp.model.AllTeamInfo;
 import com.example.livecricketapp.model.TournamentInfo;
+import com.example.livecricketapp.user.adapters.MatchesAdapter;
 import com.example.livecricketapp.user.adapters.TeamsNamesAdapter;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class TeamsAndMatches extends AppCompatActivity implements TeamsNamesAdapter.On_Click {
+public class TeamsAndMatches extends AppCompatActivity implements TeamsNamesAdapter.On_Click , MatchesAdapter.On_click {
 
     private ActivityTeamsAndMatchesBinding binding;
     private TournamentInfo info;
     private FirebaseFirestore db;
     private TeamsNamesAdapter adapter;
+    private MatchesAdapter matchesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,8 @@ public class TeamsAndMatches extends AppCompatActivity implements TeamsNamesAdap
         info = (TournamentInfo) getIntent().getSerializableExtra("info");
 
         db = FirebaseFirestore.getInstance();
+
+        set_data();
 
         binding.navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -74,6 +80,9 @@ public class TeamsAndMatches extends AppCompatActivity implements TeamsNamesAdap
         binding.startDate.setText("Start Date : " + info.getStart_date());
         binding.endDate.setText("End Date : " + info.getEnd_date());
         get_team_data();
+        get_match_data();
+        String text = "<b>NOTE : </b>You can donate participation fees on behalf of your favourite team and support them win tournament.";
+        binding.note.setText(Html.fromHtml(text));
     }
 
     private void get_team_data( )
@@ -92,8 +101,29 @@ public class TeamsAndMatches extends AppCompatActivity implements TeamsNamesAdap
                 });
     }
 
+    private void get_match_data()
+    {
+        db.collection("Match Info")
+                .document(info.getTournamentId())
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        AllMatchInfo allMatchInfo = documentSnapshot.toObject(AllMatchInfo.class);
+                        matchesAdapter = new MatchesAdapter(TeamsAndMatches.this,allMatchInfo.getMatchInfos(),TeamsAndMatches.this::move_to_match_info);
+                        binding.recyclerMatches.setAdapter(matchesAdapter);
+                        binding.recyclerMatches.setLayoutManager(new LinearLayoutManager(TeamsAndMatches.this));
+                    }
+                });
+    }
+
     @Override
     public void open_team(int a) {
+
+    }
+
+    @Override
+    public void move_to_match_info(int a) {
 
     }
 }
