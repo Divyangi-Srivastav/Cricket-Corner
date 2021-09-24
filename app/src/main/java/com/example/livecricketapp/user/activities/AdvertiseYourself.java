5f6c0cf4da31d89e9f988fbc29b673a2fd887da6
Ansh <anshtandonat@gmail.com>
2021-09-24 19:08:42 +0530

    Day 23 : Made some changes in UI

---
+++ app/src/main/java/com/example/livecricketapp/user/activities/AdvertiseYourself.java
@@ -0,0 +1,56 @@
+package com.example.livecricketapp.user.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.livecricketapp.R;
+import com.example.livecricketapp.activities.AdsRequest;
+import com.example.livecricketapp.activities.Dashboard;
+import com.example.livecricketapp.activities.HomeActivity;
+import com.example.livecricketapp.databinding.ActivityAdvertiseYourselfBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class AdvertiseYourself extends AppCompatActivity {
+
+    private ActivityAdvertiseYourselfBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAdvertiseYourselfBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch ( item.getItemId() ) {
+                    case R.id.home:
+                        Intent intent = new Intent(AdvertiseYourself.this , HomeActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.settings:
+
+                        break;
+
+                    case R.id.account:
+                        Intent intent1 = new Intent(AdvertiseYourself.this , Dashboard.class);
+                        startActivity(intent1);
+                        break;
+
+                }
+                return true;
+            }
+        });
+    }
+
+    public void back(View view) {
+        finish();
+    }
+}
