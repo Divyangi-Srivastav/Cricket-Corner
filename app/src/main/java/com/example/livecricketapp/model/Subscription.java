ce91fd2d73cab301a24fe972514f7844ba3b9517
Ansh <anshtandonat@gmail.com>
2021-09-30 19:54:15 +0530

    Day 28 : Completed UI for Buy Subscription Activity

--- app/src/main/java/com/example/livecricketapp/model/Subscription.java
+++ app/src/main/java/com/example/livecricketapp/model/Subscription.java
@@ -14,7 +14,80 @@ public class Subscription implements Serializable {
     private String validTill;
     private int money;
 
+    public Subscription ()
+    {
 
+    }
 
+    public String getUserId() {
+        return userId;
+    }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTournamentId() {
+        return tournamentId;
+    }
+
+    public void setTournamentId(String tournamentId) {
+        this.tournamentId = tournamentId;
+    }
+
+    public Boolean getMatchSubscription() {
+        return matchSubscription;
+    }
+
+    public void setMatchSubscription(Boolean matchSubscription) {
+        this.matchSubscription = matchSubscription;
+    }
+
+    public Boolean getTourSubscription() {
+        return tourSubscription;
+    }
+
+    public void setTourSubscription(Boolean tourSubscription) {
+        this.tourSubscription = tourSubscription;
+    }
+
+    public String getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(String matchId) {
+        this.matchId = matchId;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(String validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public String getValidTill() {
+        return validTill;
+    }
+
+    public void setValidTill(String validTill) {
+        this.validTill = validTill;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
