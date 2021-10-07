5a603343eba12c1d508915a9896d4e0f3a74b59e
Ansh <anshtandonat@gmail.com>
2021-10-07 13:07:01 +0530

    Day 33 : Added a new Activity HomeActivityUser

--- app/src/main/java/com/example/livecricketapp/user/activities/HomeActivityUser.java
+++ app/src/main/java/com/example/livecricketapp/user/activities/HomeActivityUser.java
@@ -1,12 +1,23 @@
 package com.example.livecricketapp.user.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import com.example.livecricketapp.R;
+import com.example.livecricketapp.activities.AdsRequest;
+import com.example.livecricketapp.activities.Dashboard;
+import com.example.livecricketapp.activities.HomeActivity;
+import com.example.livecricketapp.activities.HostMatchActivityOne;
+import com.example.livecricketapp.activities.Host_a_tournament;
 import com.example.livecricketapp.databinding.ActivityHomeUserBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class HomeActivityUser extends AppCompatActivity {
+public class HomeActivityUser extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityHomeUserBinding binding;
 
@@ -15,5 +26,55 @@ public class HomeActivityUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeUserBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        binding.btnViewATour.setOnClickListener(this::onClick);
+        binding.btnViewAMatch.setOnClickListener(this::onClick);
+        binding.btnMakeAdRequest.setOnClickListener(this::onClick);
+
+        binding.navigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch ( item.getItemId() ) {
+                    case R.id.home:
+                        Intent intent = new Intent(HomeActivityUser.this , HomeActivityUser.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.settings:
+                        Intent intent2 = new Intent(HomeActivityUser.this, Settings.class);
+                        startActivity(intent2);
+                        break;
+
+                    case R.id.account:
+                        Intent intent1 = new Intent(HomeActivityUser.this , Dashboard.class);
+                        startActivity(intent1);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.btn_view_a_match:
+                Intent intent3 = new Intent(this , HostMatchActivityOne.class);
+                startActivity(intent3);
+                break;
+            case R.id.btn_view_a_tour:
+                Intent intent = new Intent( this ,Tournaments.class );
+                startActivity(intent);
+                break;
+            case R.id.btn_make_ad_request:
+                Intent intent2 = new Intent(this, AdvertiseYourself.class);
+                startActivity(intent2);
+                break;
+        }
     }
 }
