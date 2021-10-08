f4bd44022caf8d1cd1ccc8199246d071d7671e74
Ansh <anshtandonat@gmail.com>
2021-10-08 15:26:01 +0530

    Day 34 : Added about page

---
+++ app/src/main/java/com/example/livecricketapp/common/AboutActivity.java
@@ -0,0 +1,19 @@
+package com.example.livecricketapp.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.livecricketapp.databinding.ActivityAboutBinding;
+
+public class AboutActivity extends AppCompatActivity {
+
+    private ActivityAboutBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAboutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
--- app/src/main/java/com/example/livecricketapp/user/activities/SettingsUser.java
+++ app/src/main/java/com/example/livecricketapp/user/activities/SettingsUser.java
@@ -1,16 +1,17 @@
 package com.example.livecricketapp.user.activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.livecricketapp.R;
 import com.example.livecricketapp.authentication.ForgotPasswordActivity;
+import com.example.livecricketapp.common.AboutActivity;
 import com.example.livecricketapp.databinding.ActivitySettingsUserBinding;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,9 +29,9 @@ public class SettingsUser extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                switch ( item.getItemId() ) {
+                switch (item.getItemId()) {
                     case R.id.home:
-                        Intent intent = new Intent(SettingsUser.this , HomeActivityUser.class);
+                        Intent intent = new Intent(SettingsUser.this, HomeActivityUser.class);
                         startActivity(intent);
                         break;
 
@@ -38,7 +39,7 @@ public class SettingsUser extends AppCompatActivity {
                         break;
 
                     case R.id.account:
-                        Intent intent1 = new Intent(SettingsUser.this , DashboardUser.class);
+                        Intent intent1 = new Intent(SettingsUser.this, DashboardUser.class);
                         startActivity(intent1);
                         break;
 
@@ -49,23 +50,25 @@ public class SettingsUser extends AppCompatActivity {
 
     }
 
-    public void move_to_change_Password ( View view )
-    {
-        Intent intent = new Intent(this , ForgotPasswordActivity.class);
+    public void move_to_change_Password(View view) {
+        Intent intent = new Intent(this, ForgotPasswordActivity.class);
         startActivity(intent);
     }
 
-    public void move_to_view_subscription ( View view )
-    {
-        Intent intent = new Intent(this , ViewSubscription.class);
+    public void move_to_view_subscription(View view) {
+        Intent intent = new Intent(this, ViewSubscription.class);
         startActivity(intent);
     }
 
-    public void log_out (View view)
-    {
+    public void log_out(View view) {
         FirebaseAuth.getInstance().signOut();
         this.finishAffinity();
         Toast.makeText(this, "Log Out Successful", Toast.LENGTH_SHORT).show();
+    }
+
+    public void about(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 
 }
